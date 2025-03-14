bed2264682a94a6633b46b8c4c992e4bb59c5bc3
imolene <vinicius337imolene@gmail.com>
2025-03-13 21:16:39 -0300

    deu certo

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+
+reqUrl = "https://api.github.com/octocat"
+
+headerList = {
+    "Accept": "application/vnd.github.v3+json",  # Corrigido para a versão correta da API
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "X-GitHub-Api-Version": "2022-11-28"
+}
+
+payload = ""  # O payload geralmente é usado para enviar dados com a requisição, mas nesse caso não precisa ser alterado.
+
+response = requests.get(reqUrl, headers=headerList)  # Usando requests.get para simplificar
+
+print(response.text)
